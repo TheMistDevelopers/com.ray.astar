58c26df58ca8a70ecb8fd1adec25cb61791911b3
Ray <30406361+ztFaceU@users.noreply.github.com>
2023-05-04 17:12:06 +0800

    提交编辑到处初步代码

---
+++ Core/GraphExport/GridGraphExport.cs
@@ -0,0 +1,64 @@
+using Pathfinding;
+using Pathfinding.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using System.IO;
+
+namespace ExportServer
+{
+    [RequireComponent(typeof(AstarPath))]
+
+    public class GridGraphExport : MonoBehaviour
+    {
+        [LabelText("Export Grid Graph")]
+        AstarPath astarPath;
+        List<GridGraphServerData> gridGraphServerDatas = new List<GridGraphServerData>();
+
+        [Button("InitData")]
+        private void InitData()
+        {
+            astarPath = gameObject.GetComponent<AstarPath>();
+        }
+
+        [Button("CreateDataCache")]
+        [BoxGroup("Json")]
+        private void CreateDataCache()
+        {
+            gridGraphServerDatas.Clear();
+            foreach (var graph in astarPath.graphs)
+            {
+                if (graph is GridGraph)
+                {
+                    var gridGraph = (GridGraph)graph;
+                    gridGraphServerDatas.Add(new GridGraphServerData(gridGraph));
+                }
+            }
+        }
+        [Title("MapID")]
+        [BoxGroup("Json")]
+        public int MapID;
+
+        [Button("ExportJson")]
+        [BoxGroup("Json")]
+        private void ExportJson()
+        {
+            foreach (var data in gridGraphServerDatas)
+            {
+                string dataStr = JsonUtility.ToJson(data);
+                string path = Application.dataPath + "/ExportJson/" + data.Name+".json";
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                File.WriteAllText(path, dataStr);
+            }
+        }
+
+    }
+}
+
+
---
+++ Core/GraphExport/GridGraphServerData.cs
@@ -0,0 +1,112 @@
+﻿using Pathfinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ExportServer
+{
+    [Serializable]
+    public class GridGraphServerData
+    {
+        [NonSerialized]
+        private GridGraph graph;
+        public GridGraphServerData(GridGraph graph) 
+        {
+            this.graph = graph;
+            Rotation = new Vector2(graph.rotation.x, graph.rotation.z);
+            Center = new Vector2(MathF.Round(graph.center.x), MathF.Round(graph.center.z));//四舍五入，规范设置都是整形，但是数据有时会.9999999所以这么处理
+            UnclampedSize = graph.unclampedSize;
+            NodeSize = graph.nodeSize;
+            Name = graph.name;
+            NodeDatas = new List<GridNodeServerData>();
+            graph.GetNodes((node) =>
+            {
+                GridNodeServerData data = new GridNodeServerData();
+                var gridNode = node as GridNode;
+                data.Index = gridNode.NodeInGridIndex;
+                data.Position = new Vector2(gridNode.position.x, gridNode.position.z);
+                data.Flags = gridNode.Flags;
+                data.FlagParse();
+                NodeDatas.Add(data);
+                Debug.Log(data.ToString());
+
+            });
+        }
+
+        public string Name;
+        public Vector2 Center;
+        public Vector2 Rotation;
+        public Vector2 UnclampedSize;
+        public float NodeSize;
+        public List<GridNodeServerData> NodeDatas;
+        
+
+    }
+    [Serializable]
+    public class GridNodeServerData
+    {
+        public int Index;
+        public Vector2 Position;
+        public uint Flags;
+        public uint Walkable;
+        public uint HNodeIndex;
+        public uint IsDirty;
+        public uint Tag;
+        public uint GraphIndex;
+
+        public void FlagParse()
+        {
+            Walkable = Flags & (1 << 0);
+            HNodeIndex = 0;
+            for (int i = 1; i <= 17; i++)
+            {
+                uint val = ((uint)1) << i;
+                var data = Flags & (((uint)1) << i);
+                if (data == 0)
+                {
+                    HNodeIndex = HNodeIndex & ~val;
+                }
+                else
+                {
+                    HNodeIndex = HNodeIndex | val;
+                }
+            }
+            IsDirty = Flags & (1 << 18);
+            Tag = 0;
+            for (int i = 19; i <= 23; i++)
+            {
+                uint val = ((uint)1) << i;
+                var data = Flags & (((uint)1) << i);
+                if (data == 0)
+                {
+                    Tag = Tag & ~val;
+                }
+                else
+                {
+                    Tag = Tag | val;
+                }
+            }
+            GraphIndex = 0;
+            for (int i = 24; i <= 31; i++)
+            {
+                uint val = ((uint)1) << i;
+                var data = Flags & (((uint)1) << i);
+                if (data == 0)
+                {
+                    GraphIndex = GraphIndex & ~val;
+                }
+                else
+                {
+                    GraphIndex = GraphIndex | val;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    }
+}
